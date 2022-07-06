d4d919fc5fcf96883e9e563f84f1921230328ba7
albetin4 <102990829+albetin4@users.noreply.github.com>
2022-07-06 20:38:55 +0300

    asd

---
+++ StartSeleniumQA13/src/test/java/StartTest.java
@@ -0,0 +1,21 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartTest {
+    @BeforeMethod
+    public void precondition(){
+
+    }
+
+    @Test
+    public void test(){
+
+    }
+
+
+    @AfterTest
+    public void postCondition(){
+
+    }
+}
