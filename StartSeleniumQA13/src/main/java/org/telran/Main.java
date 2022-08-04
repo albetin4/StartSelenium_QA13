446445a4a1b4134c6f38ad585b7a585ec8b40c2c
albetin4 <102990829+albetin4@users.noreply.github.com>
2022-08-04 14:04:05 +0300

    lesson5-6

--- StartSeleniumQA13/src/main/java/org/telran/Main.java
+++ StartSeleniumQA13/src/main/java/org/telran/Main.java
@@ -2,6 +2,13 @@ package org.telran;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a = 5;
+        if(a == 5){
+            switch(a){
+                case 5:
+                    System.out.println(a);
+            }
+        }
+
     }
 }
--- StartSeleniumQA13/src/test/java/StartTest.java
+++ StartSeleniumQA13/src/test/java/StartTest.java
@@ -1,21 +1,85 @@
-import org.testng.annotations.AfterTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class StartTest {
+    WebDriver wd;
+
     @BeforeMethod
-    public void precondition(){
+    public void preCondition(){
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+    wd.get("https://contacts-app.tobbymarshall815.vercel.app/home");
+//        wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+// chain
+        // wd.navigate().forward()
+        // wd.navigate().back()
+        // wd.navigate().refresh()
 
     }
 
     @Test
-    public void test(){
+    public void testName(){
+        WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> elements = wd.findElements(By.tagName("div"));
+        System.out.println(elements.size());
+//        // strategies - high level
+        element = wd.findElement(By.linkText("ABOUT")); // old school -high resource consumption
+        element = wd.findElement(By.partialLinkText("ABO")); // old school - high resource consumption
+        element = wd.findElement(By.id("root"));
+        element = wd.findElement(By.className("container"));
+
+//        // cssSelector - low level
+//        // by tagName
+        element = wd.findElement(By.cssSelector("div"));
+//        // by className
+        element = wd.findElement(By.cssSelector(".container"));
+//        // by ID
+        element = wd.findElement(By.cssSelector("#root"));
+//        // by attribute
+        element = wd.findElement(By.cssSelector("[placeholder='Email']"));
+//        // by attribute starts with
+        element = wd.findElement(By.cssSelector("[href^='Ho']"));
+//        // by attribute ends with
+        element = wd.findElement(By.cssSelector("[href$='out']"));
+//        // by attribute contains
+        element = wd.findElement(By.cssSelector("[href*='ogi']"));
+//
+//        // xPath selectors:
+//        // by tagName
+//        element = wd.findElement(By.xpath("//div"));
+//        // by className
+//        element = wd.findElement(By.xpath("//*[@class='container']"));
+//        // by ID
+//        element = wd.findElement(By.xpath("//*[@id='root']"));
+//        // by attribute
+//        element = wd.findElement(By.xpath("//*[@placeholder='Email']"));
+//        // by attribute starts with
+//        element = wd.findElement(By.xpath("//*[starts-with(@href,'/Ho')]"));
+//        // by attribute ends with - unavailable
+//        //element = wd.findElement(By.cssSelector("[href$='out']"));
+//        // by attribute contains
+//        element = wd.findElement(By.xpath("//*[contains(@href,'ogi')]"));
 
     }
 
+    @Test
+    public void testPassword(){
 
-    @AfterTest
+    }
+
+    @AfterMethod
     public void postCondition(){
+
+        // wd.close(); // not closed additional tabs, only current
+        wd.quit();
 
     }
 }
